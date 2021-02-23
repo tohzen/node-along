3bc01fab4552e62c43904aff395e5c433c1cf644
Tohzen <melvin.l.mobley@gmail.com>
2021-02-23 15:44:28 -0500

    finished

--- mental-age.js
+++ mental-age.js
@@ -1,1 +1,17 @@
 const getInput = require("./get-input.js");
+
+const UserAge = getInput();
+if (UserAge === "--help") {
+    console.log("This function returns your mental age.");
+    console.log('Please enter your age after "node mental-age.js" and a space.');
+}
+
+const MentalAge = UserAge / 2 + 3;
+const ageNextYear = Number(UserAge) + 1;
+console.log("Next year you'll be " + ageNextYear + " years old")
+
+console.log("You are" + UserAge + "years old.");
+
+console.log("your mental age is " + MentalAge + "years old.");
+
+console.log(MentalAge);
