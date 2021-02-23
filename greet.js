950b590e7575422d71d1f3063188025ae857637b
Tohzen <melvin.l.mobley@gmail.com>
2021-02-23 15:43:42 -0500

    finished

--- greet.js
+++ greet.js
@@ -1,1 +1,8 @@
 const getInput = require("./get-input.js");
+
+// use console.log to print out a greeting to a user
+//
+// 1. get the user's name and save it in a variable getInput()
+const str = getInput();
+// 2. print out what's in that variable
+console.log("Hello " , str, ".", "What", "are", "you", "doing?");
